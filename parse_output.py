5c0e5102fb8190b8c9d4945011573fb326108691
veggiebob <veggiebobowman@gmail.com>
2022-09-01 08:57:04 -0400

    bruh

--- parse_output.py
+++ parse_output.py
@@ -24,8 +24,8 @@ def summarize(data: pd.DataFrame):
 	ps = []
 	for (idx, r) in data.iterrows():
 		# print(r)
-		p = dict(x=(r.day + day_ftime(r.time.time()), 3),
-				 y=(r.lower + r.upper, 3))
+		p = dict(x=r.day + day_ftime(r.time.time()),
+				 y=r.lower + r.upper)
 		ps.append(p)
 	return ps
 
