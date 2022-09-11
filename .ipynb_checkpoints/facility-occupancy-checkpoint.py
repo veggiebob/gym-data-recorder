d75959f5f6cc78bae200c34c8b369a26fb33350c
veggiebob <veggiebobowman@gmail.com>
2022-09-11 09:55:42 -0400

    edit gitignore

--- .ipynb_checkpoints/facility-occupancy-checkpoint.py
+++
@@ -1,9 +0,0 @@
-import datetime
-from time import sleep
-import fetch_data
-
-while True:
-  data = fetch_data.read_csv_data()
-  new_data = fetch_data.add_datapoint(data)
-  fetch_data.save_data(new_data)
-  sleep(10 * 60) # only record every 10 minutes
